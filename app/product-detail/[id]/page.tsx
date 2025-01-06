cf95948d3044ea11029f384a35c6b57b97f99824
Adeena-Imran <adeenaimran01@gmail.com>
2025-01-06 16:29:15 +0500

    fifth commit

--- app/product-detail/[id]/page.tsx
+++ app/product-detail/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { use } from 'react';
 import Image from 'next/image';
 
 interface Product {
@@ -16,31 +17,31 @@ async function getProduct(id: string): Promise<Product> {
   return response.json();
 }
 
-
-const ProductDetails = async ({ params }: { params: { id: string } }) => {
-
-  const product = await getProduct(params.id);
+const ProductDetails = ({ params }: { params: { id: string } }) => {
+  const products = use(getProduct(params.id));
 
   return (
     <div className="min-h-screen bg-gray-100 py-10">
       <div className="max-w-screen-lg mx-auto p-4 bg-white rounded-lg shadow-md">
         <div className="flex flex-col md:flex-row gap-8">
-          {/* Image */}
+          {/*Image*/}
           <div className="flex-1">
             <Image
-              width={500}
-              height={500}
-              src={product.image}
-              alt={product.title}
+              width={100}
+              height={100}
+              src={products.image}
+              alt={products.title}
               className="object-cover w-full h-auto rounded-t-lg"
             />
           </div>
 
-          {/* Details */}
+          {/*Details*/}
           <div className="flex-1">
-            <h1 className="text-2xl font-semibold text-yellow-950">{product.title}</h1>
-            <p className="text-lg mt-4 text-yellow-950">{product.description}</p>
-            <p className="mt-6 text-xl font-bold text-green-600">${product.price}</p>
+            <h1 className="text-2xl font-semibold text-yellow-950">{products.title}</h1>
+            <p className="text-lg mt-4 text-yellow-950">{products.description} </p>
+            <p className="mt-6 text-xl font-bold text-green-600">${products.price}</p>
+            
+          
           </div>
         </div>
       </div>
@@ -51,3 +52,5 @@ const ProductDetails = async ({ params }: { params: { id: string } }) => {
 export default ProductDetails;
 
 
+
+ 
